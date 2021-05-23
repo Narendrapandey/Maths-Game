f9b89949e5fc998c4c3e78ced8a2b53c348c8881
Narendra Pandey <>
2021-05-23 15:22:52 +0530

    initial commit

---
+++ MathsGame/Shared/MathsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathsGameApp.swift
+//  Shared
+//
+//  Created by Narendra Pandey on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MathsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
